9fe943242d57005b818e0e3c0a17c9d95e247844
Leon Jamison <leonjamison@icloud.com>
2019-11-24 23:53:32 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,10 @@
 def display_board
   puts "Tic Tac Toe Board"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
+    puts "-----------"
+    puts "   |   |   "
 end
 
-display_board
+
